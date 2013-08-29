2272e87ff54fe20fc4b8697d334027d5a9f1f739
Og B. Maciel <ogmaciel@gnome.org>
2013-08-29 16:28:12 -0400

    Return a warning message if the ssh key cannot be found.

--- tests/basetest.py
+++ tests/basetest.py
@@ -106,7 +106,10 @@ class BaseTest(unittest.TestCase):
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh_client.connect(self.host, username=self.root, key_filename=self.ssh_key)
+        try:
+            ssh_client.connect(self.host, username=self.root, key_filename=self.ssh_key)
+        except IOError, e:
+            return "Make sure that your ssh key exists and is accessible."
 
         stdin, stdout, stderr = ssh_client.exec_command('uptime')
 
