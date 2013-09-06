09e3c5a40ff3ee1e86db99c66c38712a14c2528e
Og B. Maciel <ogmaciel@gnome.org>
2013-09-06 18:51:33 -0400

    Test creation of users with a default locale.

--- tests/test_Users.py
+++ tests/test_Users.py
@@ -25,6 +25,23 @@ class TestUsers(BaseTest):
         self.assertEqual(user['username'], username)
         self.assertEqual(user['email'], useremail)
 
+    def test_create_user_3(self):
+        "Create user with default locale"
+
+        user = self.user_api.create(default_loc='pt-BR')
+        self.assertEqual(user, self.user_api.user(user['id']))
+
+        user = self.user_api.user(user['id'])
+        self.assertEqual('pt-BR', user['preferences']['user']['locale'])
+
+    def test_create_user_4(self):
+        "Re-creates system user."
+
+        user = self.user_api.create()
+        self.assertEqual(user, self.user_api.user(user['id']))
+
+        self.assertRaises(ServerRequestError, lambda: self.user_api.create(name=user['username']))
+
     def test_invalid_usernames(self):
         "Fail username validation"
 
@@ -64,14 +81,6 @@ class TestUsers(BaseTest):
             user = self.user_api.create(name=name)
             self.assertEqual(user, self.user_api.user(user['id']))
 
-    def test_create_user_5(self):
-        "Re-creates system user."
-
-        user = self.user_api.create()
-        self.assertEqual(user, self.user_api.user(user['id']))
-
-        self.assertRaises(ServerRequestError, lambda: self.user_api.create(name=user['username']))
-
     def test_update_user_email_and_succeed_1(self):
         "Creates and updates a system user's email."
 
@@ -93,6 +102,17 @@ class TestUsers(BaseTest):
         # Nothing should have changed
         self.assertEqual(user, self.user_api.user(user['id']))
 
+    def test_update_user_locale_1(self):
+        "Creates user and updates the default locale"
+
+        user = self.user_api.create()
+        self.assertEqual(user, self.user_api.user(user['id']))
+
+        self.user_api.update(user['id'], **{'default_locale': 'pt-BR'})
+        user = self.user_api.user(user['id'])
+        self.assertEqual('pt-BR', user['preferences']['user']['locale'])
+
+        ['preferences']['user']['locale']
     def test_delete_user_1(self):
         "Creates and deletes a system user."
 
